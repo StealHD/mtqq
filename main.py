d4fee53872449010be04ca4792a768cb46439500
StealHD <1099631220@qq.com>
2023-11-24 17:10:22 +0800

    add goldprice v0.0.3 asynchronous execution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import time
 import json
@@ -10,13 +11,17 @@ topic = "homeassistant/goldprice"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
+
 def connect_mqtt():
+    """
+
+    :return: mqtt client
+    """
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-
     client = mqtt_client.Client(client_id)
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -24,11 +29,15 @@ def connect_mqtt():
 
 
 def publish(client):
+    """
+
+    :param client:
+    :return: topic to mqtt server
+    """
     while True:
         goldInfo = GoldSensor().update()
         msg = goldInfo
         result = client.publish(topic, json.dumps(msg, ensure_ascii=False))
-        # result: [0, 1]
         status = result[0]
         if status == 0:
             print(f"Send {msg} to topic `{topic}`")
@@ -37,12 +46,12 @@ def publish(client):
         time.sleep(3600)
 
 
-
-def run():
+async def main():
     client = connect_mqtt()
-    client.loop_start()
     publish(client)
+    # mqtt_client = AsyncMQTTClient(MQTT_BROKER, MQTT_PORT, MQTT_TOPIC)
+    await client.loop_start()
+    mqtt_client.stop()
 
-
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    asyncio.run(main())
